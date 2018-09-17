b573e898079058101464af48dc859d1ca0396046
wangleikai <328909605@qq.com>
2018-09-17 17:56:54 +0800

    Initial commit

---
+++ src/main/scala/WordCount.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.{SparkConf, SparkContext}
+
+/**
+  * @author WangLeiKai
+  *         2018/9/17  16:56
+  */
+object WordCount {
+  def main(args: Array[String]): Unit = {
+    val conf = new SparkConf()
+    val sc = new SparkContext(conf)
+
+    sc.textFile(args(0))
+      .flatMap(_.split(" "))
+      .map((_,1)).reduceByKey(_+_)
+      .sortBy(_._2,false)
+      .saveAsTextFile(args(1))
+
+    sc.stop()
+  }
+
+}
